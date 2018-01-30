057f0ea0a8828123b0db41cf89721bb57b917044
Pol Bas Mantilla <pbas@zemsania.com>
2018-01-30 17:14:23 +0100

    error handling ep10

---
+++ index.js
@@ -0,0 +1,33 @@
+'user strict'
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+// set up express app
+const app = express()
+
+// Conect to mongoDB
+mongoose.connect('mongodb://localhost/ninjago')
+mongoose.Promise = global.Promise // because deprecated
+
+// PArse data to JSON
+app.use(bodyParser.json())
+
+// inicialize routes
+app.use('/api', require('./routes/api'))
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    // console.log(err)
+    res.status(422).send({ error: err.message })
+})
+
+// port listening to
+const PORT = process.env.port || 3500
+
+
+// listen for requests
+app.listen(PORT, () => {
+    console.log('now listening for requests')
+})
---
+++ routes/api.js
@@ -0,0 +1,30 @@
+'user strict'
+
+const express = require('express')
+const router = express.Router()
+const Ninja = require('../models/ninja')
+
+// Get a list of ninjas from the db
+router.get('/ninjas', (req, res, next) => {
+    res.send({ type: 'GET' })
+})
+
+// Add a ninja to db
+router.post('/ninjas', (req, res, next) => {
+    Ninja.create(req.body).then((ninja) => {
+        res.send(ninja)
+    }).catch(next)
+})
+
+// Update a ninja in the db
+router.put('/ninjas/:id', (req, res, next) => {
+    res.send({ type: 'PUT' })
+})
+
+// Delete a ninja from de db
+router.delete('/ninjas/:id', (req, res, next) => {
+    res.send({ type: 'DELETE' })
+})
+
+
+module.exports = router
